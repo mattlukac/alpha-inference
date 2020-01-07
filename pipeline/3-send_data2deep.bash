1f7ae40d70c7b58da50cc9dafe2dbb6670010c65
Matt Lukac <mlukac@talapas-ln2.cm.cluster>
2020-01-07 14:53:12 -0800

    added input for popName

--- pipeline/3-send_data2deep.bash
+++ pipeline/3-send_data2deep.bash
@@ -2,4 +2,6 @@
 
 # this script sends relevant data to deep, 
 
-sshpass -p "bearclaw" rsync -avzhe ssh ../sims/ceu/trainingData mlukac@deep.uoregon.edu:/data0/mlukac/ceu/
+pop=$1
+
+sshpass -p "bearclaw" rsync -avzhe ssh ../sims/${pop}/trainingData mlukac@deep.uoregon.edu:/data0/mlukac/${pop}/
