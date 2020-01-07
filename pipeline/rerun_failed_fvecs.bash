91217194ebe48c34807bf8fcc3cabb77fb463287
Matt Lukac <mlukac@talapas-ln2.cm.cluster>
2020-01-07 14:31:10 -0800

    generalized for popName

--- pipeline/rerun_failed_fvecs.bash
+++ pipeline/rerun_failed_fvecs.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+pop=$1
 source config.txt
 lines="$alphaDraws "
 
-wc -l ../sims/ceu/fvecs/new/fvec* | grep -v $lines | grep -oP '(?<=/new/fvecs).*?(?=.tsv)' > failed_jobs.txt
+wc -l ../sims/${pop}/fvecs/new/fvec* | grep -v $lines | grep -oP '(?<=/new/fvecs).*?(?=.tsv)' > failed_jobs.txt
 while read jobNum; do 
-  rm ../sims/ceu/alpha/alpha_values${jobNum}.tsv
-  rm ../sims/ceu/discout/discout${jobNum}/*
-  rm ../sims/ceu/fvecs/new/fvecs${jobNum}.tsv
-  rm ../sims/ceu/params/params${jobNum}.tsv
-  sbatch 1-discoal_sims.sbatch ${jobNum} 
+  rm ../sims/${pop}/alpha/alpha_values${jobNum}.tsv
+  rm ../sims/${pop}/discout/discout${jobNum}/*
+  rm ../sims/${pop}/fvecs/new/fvecs${jobNum}.tsv
+  rm ../sims/${pop}/params/params${jobNum}.tsv
+  sbatch 1-discoal_sims.sbatch ${jobNum} ${pop}
 done < failed_jobs.txt
 
 rm failed_jobs.txt
