099d0cbc0b22ccd4aec35b7e43a4fe6559a35e14
Matt Lukac <mattmlukac@gmail.com>
2020-01-11 15:51:46 -0800

    remove deep login

--- pipeline/3-send_data2deep.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# this script sends relevant data to deep, 
-
-pop=$1
-
-sshpass -p "bearclaw" rsync -avzhe ssh ../sims/${pop}/trainingData mlukac@deep.uoregon.edu:/data0/mlukac/${pop}/
