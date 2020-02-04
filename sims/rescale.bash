abb1ff4be1478ba83dc9fa2ce3e037b15bb2387f
Matt Lukac <mattmlukac@gmail.com>
2020-02-03 16:08:12 -0800

    put in appropriate directory

--- sims/rescale.bash
+++ sims/rescale.bash
@@ -5,31 +5,32 @@
 ## and 'zoom in' to a smaller window of length l
 ## then save the new file with prefix sliced
 
-# index for msOut file
-i=$1
-rescaledWindowSize=$2
-bigWindowSize=$(head -n 1 alpha${i}.msOut | cut -f 4 -d ' ')
+pop=$1
+i=$2 #index for msOut file
+rescaledWindowSize=$3
+discout=$pop/discout/discout$i
+bigWindowSize=$(head -n 1 $discout/alpha${i}.msOut | cut -f 4 -d ' ')
 headerLines=5
-sampleSize=$(wc -l < alpha${i}.msOut)
+sampleSize=$(wc -l < $discout/alpha${i}.msOut)
 sampleSize=$(($sampleSize - $headerLines - 1))
 
 # chop up msOut file into header, pos, genos
-head -n $headerLines alpha${i}.msOut > alpha${i}.msOut.header
-head -n $(($headerLines+1)) alpha${i}.msOut | tail -n 1 | cut -f 2- -d ' ' > alpha${i}.msOut.positions
-tail -n $sampleSize alpha${i}.msOut > alpha${i}.msOut.genos
+head -n $headerLines $discout/alpha${i}.msOut > $discout/alpha${i}.msOut.header
+head -n $(($headerLines+1)) $discout/alpha${i}.msOut | tail -n 1 | cut -f 2- -d ' ' > $discout/alpha${i}.msOut.positions
+tail -n $sampleSize $discout/alpha${i}.msOut > $discout/alpha${i}.msOut.genos
 
 # use python to slice positions and genos 
-python rescale.py alpha${i}.msOut.positions alpha${i}.msOut.genos $bigWindowSize $rescaledWindowSize
+python rescale.py $discout/alpha${i}.msOut.positions $discout/alpha${i}.msOut.genos $bigWindowSize $rescaledWindowSize
 
 # add 'positions: ' back into sliced positions
-awk '{print "positions: "$0}' slicedalpha${i}.msOut.positions > slicedalpha${i}.msOut.positions
+awk '{print "positions: "$0}' $discout/slicedalpha${i}.msOut.positions > $discout/slicedalpha${i}.msOut.positions
 
 # sliced files will have 'sliced' prefix
 # recombine them to make sliced msOut file
-outFile=sliced${rescaledWindowSize}_alpha${i}.msOut
-cat alpha${i}.msOut.header > $outFile
-cat slicedalpha${i}.msOut.positions >> $outFile
-cat slicedalpha${i}.msOut.genos >> $outFile
+outFile=$discout/sliced${rescaledWindowSize}_alpha${i}.msOut
+cat $discout/alpha${i}.msOut.header > $outFile
+cat $discout/slicedalpha${i}.msOut.positions >> $outFile
+cat $discout/slicedalpha${i}.msOut.genos >> $outFile
 
 # clean directory
-rm alpha${i}.msOut.* slicedalpha${i}.msOut.*
+rm $discout/alpha${i}.msOut.* $discout/slicedalpha${i}.msOut.*
